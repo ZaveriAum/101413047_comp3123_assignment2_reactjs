e4fb4252843b9784b0d70d2f614d3ce40a06fedb
Aum Zaveri <aumzaveri06@gmail.com>
2024-11-18 15:34:25 -0500

    delete employee login completed

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -10,8 +10,10 @@ const Employee = () => {
   const navigate = useNavigate();
   const [employee, setEmployee] = useState([]);
   const [query, setQuery] = useState()
-  const [selectedEmployee, setSelectedEmployee] = useState(null);
-  const [modalShow, setModalShow] = useState(false);
+  const [selectedDetailsEmployee, setSelectedDetailsEmployee] = useState(null);
+  const [modalDetailsShow, setModalDetailsShow] = useState(false);
+  const [selectedDeleteEmployee, setSelectedDeleteEmployee] = useState(null);
+  const [modalDeleteShow, setModalDeleteShow] = useState(false);
 
   const getEmployees = async () => {    
     await apiClient
@@ -34,6 +36,15 @@ const Employee = () => {
         setEmployee(emps.data.employees);
     })
     .catch((e) => console.log(e));
+  }
+
+  const deleteEmployee = async (eid)=>{
+    setModalDeleteShow(false);
+    await apiClient
+    .delete(`/api/v1/emp/employees?eid=${eid}`)
+    .then((res)=>{
+        getEmployees();
+    }).catch(e=>console.log(e))
   }
 
   const EmployeeDetailsModal = ({ employee, show, onHide }) => {
@@ -59,6 +70,38 @@ const Employee = () => {
     );
   };
 
+  const EmployeeDeleteModal = ({ employee, show, onHide }) => {
+    if (!employee) return null;
+
+    return (
+      <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Are you sure you want to delete this employee?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p><strong>Name: </strong>{employee.first_name} {employee.last_name}</p>
+          <p><strong>Email:</strong> {employee.email}</p>
+          <p><strong>Position:</strong> {employee.position}</p>
+          <p><strong>Department:</strong> {employee.department}</p>
+          <p><strong>Joining Date:</strong> {new Date(employee.date_of_joining).toLocaleDateString()}</p>
+          <p><strong>Created:</strong> {new Date(employee.created_at).toLocaleDateString()}</p>
+          <p><strong>Updated:</strong> {new Date(employee.updated_at).toLocaleDateString()}</p>
+          <p><strong>Salary:</strong> ${employee.salary}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={(e) => 
+            {
+                e.stopPropagation();
+                deleteEmployee(employee._id)
+            }
+          }>Delete</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   return (
     <div className="empBody">
         <InputGroup name="query" type="query" onChange={(e) => setQuery(e.target.value)}>
@@ -70,8 +113,8 @@ const Employee = () => {
           key={emp._id}
           style={{ width: '18rem', cursor: 'pointer', marginBottom: '10px' }}
           onClick={() => {
-            setSelectedEmployee(emp);
-            setModalShow(true);
+            setSelectedDetailsEmployee(emp);
+            setModalDetailsShow(true);
           }}
         >
           <Card.Body>
@@ -80,19 +123,33 @@ const Employee = () => {
             </Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{emp.position}</Card.Subtitle>
             <Card.Subtitle className="mb-2 text-muted">{emp.department}</Card.Subtitle>
-            <Button variant="primary" className="me-2" onClick={()=>navigate(`/employees/${emp._id}`)}>
+            <Button variant="primary" className="me-2" onClick={(e)=>
+            {
+                e.stopPropagation();
+                navigate(`/employees/${emp._id}`)
+            }
+                }>
               <FontAwesomeIcon icon={faPenToSquare} />
             </Button>
-            <Button variant="danger">
+            <Button variant="danger" onClick={(e)=>{
+                e.stopPropagation();
+                setSelectedDeleteEmployee(emp);
+                setModalDeleteShow(true);
+            }}>
               <FontAwesomeIcon icon={faTrash} />
             </Button>
           </Card.Body>
         </Card>
       ))}
       <EmployeeDetailsModal
-        employee={selectedEmployee}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
+        employee={selectedDetailsEmployee}
+        show={modalDetailsShow}
+        onHide={() => setModalDetailsShow(false)}
+      />
+      <EmployeeDeleteModal
+      employee={selectedDeleteEmployee}
+      show={modalDeleteShow}
+      onHide={() => setModalDeleteShow(false)}
       />
     </div>
   );
